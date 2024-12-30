e7954cafcf7e981ff4cb8eaf8c6565dd73154357
Waleed Kayani <waleedkayani14@yahoo.com>
2024-12-31 01:16:56 +0500

    Add layout and navbar components for improved navigation and structure

---
+++ src/app/resume/layout.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/navbar";
+import React from "react";
+
+export default function layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="bg-green-100">
+      {/* <Navbar /> */}
+      <> {children}</>
+    </div>
+  );
+}
