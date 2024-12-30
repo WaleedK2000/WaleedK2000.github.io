c3ea3203f74d8701247c7fa22a1cd11f1ba283e9
Waleed Kayani <waleedkayani14@yahoo.com>
2024-12-31 01:16:48 +0500

    Add education and work experience data for resume section

---
+++ src/data/workExperienceData.ts
@@ -0,0 +1,31 @@
+export const workExperienceData = [
+  {
+    position_title: "Full-Stack Engineer",
+    company_name: "Codeaza Technologies",
+    location: "Islamabad, Pakistan",
+    start_date: "May 2024",
+    end_date: "Present",
+    responsibilities: [
+      "Designed and developed reusable React JS components, enhancing code modularity, reducing duplication, and improving application maintainability.",
+      "Implemented visually appealing and user-friendly UI designs from figma.",
+      "Built robust backend logic using Python & FAST API, handling complex business rules and data processing.",
+      "Developed github workflow and pulumi pipelines for CI/CD and automated deployment of applications on AWS.",
+      "Implemented visually appealing and user-friendly UI designs.",
+      "Improved API loading times by around 25% through strategic indexing and materialized view implementation, resulting in enhanced user experience and increased system efficiency.",
+      "Engineered an AI-powered solution for processing and analyzing document structures, leading to roughly 50% enhancement in module performance",
+    ],
+  },
+  {
+    position_title: "Associate Software Engineer",
+    company_name: "Prismware Technologies",
+    location: "Islamabad, Pakistan",
+    start_date: "May 2024",
+    end_date: "Present",
+    responsibilities: [
+      "Developed and optimized high-quality, testable, and reusable code for React applications.",
+      "Improved user experience and performance by implementing responsive design and optimization techniques.",
+      "Collaborated with team members to design and implement solutions to fulfill customer needs.",
+      "Improved responsiveness by about 60% using breakpoints and mobile first approach.",
+    ],
+  },
+];
