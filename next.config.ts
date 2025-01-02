5b54d3792c43df6bd202921b22a85f0f3c3c8d80
Waleed Kayani <waleedkayani14@yahoo.com>
2025-01-02 16:02:04 +0500

    Remove base path and asset prefix configuration from Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/waleedk2000.github.io",
-  assetPrefix: "/waleedk2000.github.io/",
-
-  trailingSlash: true,
 };
 
 export default nextConfig;
