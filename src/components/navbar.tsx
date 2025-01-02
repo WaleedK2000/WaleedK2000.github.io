905afb73e5bcf92b85909f524f935f7bd817c479
Waleed Kayani <waleedkayani14@yahoo.com>
2025-01-03 00:08:12 +0500

    Refactor navbar component to use <ul> for list items for better semantic structure

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,7 +10,7 @@ export default function navbar() {
         <h2>W.K</h2>
       </div>
       <div className="flex flex-row  ">
-        <div className="my-auto  flex flex-row list-none gap-10 text-primary_text_color  ">
+        <ul className="my-auto  flex flex-row list-none gap-10 text-primary_text_color  ">
           <li className="">
             <Link href={"/home"}>Home</Link>
           </li>
@@ -19,7 +19,7 @@ export default function navbar() {
           </li>
           <li>Project Portfolio</li>
           <li>Magic Moments</li>
-        </div>
+        </ul>
       </div>
       <div className="my-auto">
         <button className="bg-green-200">Dark Mode</button>
