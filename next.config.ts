a908ea1218182ebc466a10528e60300504fcb7fb
Waleed Kayani <waleedkayani14@yahoo.com>
2025-01-02 15:55:42 +0500

    Configure base path and asset prefix for GitHub Pages deployment, and set homepage in package.json

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/waleedk2000.github.io",
+  assetPrefix: "/waleedk2000.github.io/",
+
+  trailingSlash: true,
 };
 
 export default nextConfig;
