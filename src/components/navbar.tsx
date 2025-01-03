bf9e8ddb59d4f5053ec46efcfba4a74e4a37cd88
Waleed Kayani <waleedkayani14@yahoo.com>
2025-01-03 14:14:37 +0500

    Refactor navbar component to use div for navigation links and remove unused list items

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,16 +10,14 @@ export default function navbar() {
         <h2>W.K</h2>
       </div>
       <div className="flex flex-row  ">
-        <ul className="my-auto  flex flex-row list-none gap-10 text-primary_text_color  ">
-          <li className="">
-            <Link href={"/home"}>Home</Link>
-          </li>
-          <li>
-            <Link href={"/resume"}>Resume</Link>
-          </li>
-          <li>Project Portfolio</li>
-          <li>Magic Moments</li>
-        </ul>
+        <div className="my-auto  flex flex-row list-none gap-10 text-primary_text_color  ">
+          <Link href={"/home"}>Home</Link>
+
+          <Link href={"/resume"}>Resume</Link>
+
+          {/* <li>Project Portfolio</li>
+          <li>Magic Moments</li> */}
+        </div>
       </div>
       <div className="my-auto">
         <button className="bg-green-200">Dark Mode</button>
