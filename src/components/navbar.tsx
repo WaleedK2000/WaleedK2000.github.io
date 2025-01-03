5ac49b7f95e0b292f4adc5c477f887891ce704ec
Waleed Kayani <waleedkayani14@yahoo.com>
2025-01-04 00:54:17 +0500

    Refactor Navbar component to use router for navigation and improve structure

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,25 +1,25 @@
 "use client";
 
-import Link from "next/link";
 import React from "react";
 
-export default function navbar() {
+import { useRouter } from "next/navigation";
+
+export default function Navbar() {
+  const router = useRouter();
+
   return (
     <div className="nav-bar-panel  ">
       <div className="my-auto">
         <h2>W.K</h2>
       </div>
       <div className="flex flex-row  ">
-        <div className="my-auto  flex flex-row list-none gap-10 text-primary_text_color  ">
-          <Link href={"/home"} passHref={true} key={"home"}>
-            {"Home"}
-          </Link>
-
-          <Link href={"/resume"}>Resume</Link>
+        <ul className="my-auto  flex flex-row list-none gap-10 text-primary_text_color  ">
+          <li onClick={() => router.push("/home")}>Home</li>
+          <li onClick={() => router.push("/resume")}>Resume</li>
 
           {/* <li>Project Portfolio</li>
           <li>Magic Moments</li> */}
-        </div>
+        </ul>
       </div>
       <div className="my-auto">
         <button className="bg-green-200">Dark Mode</button>
